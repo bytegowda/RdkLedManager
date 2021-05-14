f1ae6ca9b495827e8b3e7b5e3c5b054b57dc32b1
Gautham Durairaj <gautham.durairaj@sky.uk>
2021-05-14 12:59:30 +0000

    RDKB-35540 : Create RDK LED Manager
    Reason for change: Introducing a new RdkLedManager
    Test Procedure:
    	* Internet connectivity to box.
    	* LED color should be set appropriatly
    	* LAN/Wifi clients should get IP
    Risks: Moderate
    Signed-off-by: gautham.durairaj@sky.uk
    
    Change-Id: I48f82609ecb3f0cdde8134dfafc304e7a1df0aee
    (cherry picked from commit 31e32d5b8111fb7afc78a1bf2502181ba225f99f)

---
+++ source/led_manager.c
@@ -0,0 +1,232 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2020 Sky
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * Copyright [2014] [Cisco Systems, Inc.]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "led_manager.h"
+#include "led_manager_global.h"
+#include "led_manager_utils.h"
+#include "led_manager_events.h"
+#include "led_manager_dbus_utils.h"
+#include "breakpad_wrapper.h"
+
+#define DEBUG_INI_NAME    "/etc/debug.ini"
+
+extern char * pComponentName;
+extern ANSC_HANDLE bus_handle;
+extern char g_Subsystem[32];
+extern PCOMPONENT_COMMON_LED_MANAGER g_pComponentCommonLedMgr;
+
+
+static void ledmgr_start ()
+{
+    CcspTraceInfo(("%s %d: LedManager Start\n", __FUNCTION__, __LINE__));
+    FILE * fd = NULL;
+
+    fd = fopen("/var/tmp/ledmanager.pid", "w+");
+    if ( !fd )
+    {
+        CcspTraceWarning(("Create /var/tmp/ledmanager.pid error. \n"));
+        return;
+    }
+    else
+    {
+        char cmd[BUFLEN_8] = {0};  
+        sprintf(cmd, "%d", getpid());
+        fputs(cmd, fd);
+        fclose(fd);
+    }
+
+    char filepath[BUFLEN_128] = {0};
+
+    if (platform_hal_initLed(filepath) != SUCCESS)
+    {
+        CcspTraceError(("%s %d: Cannot get config file. Exiting..\n", __FUNCTION__, __LINE__));
+        goto FAIL;
+    }
+
+    FILE * config_file = fopen(filepath, "r");
+
+    if (config_file == NULL)
+    {
+        CcspTraceError(("%s %d: Cannot read config file. Exiting..\n", __FUNCTION__, __LINE__));
+        goto FAIL;
+    }
+
+    char * buffer = NULL;
+    buffer = ledmgr_read_config_file(config_file);
+
+    if (buffer == NULL)
+    {
+        CcspTraceError(("%s %d: Cannot read config file. Exiting..\n", __FUNCTION__, __LINE__));
+        goto FAIL;
+    }
+
+    CcspTraceInfo(("%s %d: Reading config file successful\n", __FUNCTION__, __LINE__));
+
+    if (ledmgr_parse_config_file(buffer) != SUCCESS)
+    {
+        CcspTraceError(("%s %d: Cannot parse config file. Exiting..\n", __FUNCTION__, __LINE__));
+        free (buffer);
+        goto FAIL;
+    }
+
+    free (buffer); // buffer can be freed, config file is parsed and program has saved the data
+
+    ledmgr_print_data_to_logfile();
+
+    if (ledmgr_catch_events() != SUCCESS)
+    {
+        CcspTraceError(("%s %d: Event listener init failed.\n", __FUNCTION__, __LINE__));
+        goto FAIL;
+    }
+
+    ledmgr_free_data();
+
+FAIL:
+    ledmgr_free_data();
+}
+
+int main(int argc, char* argv[])
+{
+
+    int   cmdChar = 0;
+    pComponentName = COMPONENT_NAME_LEDMANAGER;
+
+#ifdef FEATURE_SUPPORT_RDKLOG
+    RDK_LOGGER_INIT();
+#endif
+
+    BOOL                bRunAsDaemon = TRUE;
+    int                 idx = 0;
+
+    for(idx = 1; idx < argc; idx++)
+    {
+        if((strcmp(argv[idx], "-subsys") == 0))
+        {
+            if((idx + 1) < argc)
+            {
+                AnscCopyString(g_Subsystem, argv[idx+1]);
+            }
+            else
+            {
+                CcspTraceError(("parameter after -subsys is missing"));
+            }
+        }
+        else if ( strcmp(argv[idx], "-c") == 0 )
+        {
+            bRunAsDaemon = FALSE;
+        }
+    }
+
+#if defined(_ANSC_WINDOWSNT)
+    AnscStartupSocketWrapper(NULL);
+
+    cmd_dispatch('e');
+
+    RDKLogEnable = GetLogInfo(bus_handle,"eRT.","Device.LogAgent.X_RDKCENTRAL-COM_LoggerEnable");
+    RDKLogLevel = (char)GetLogInfo(bus_handle,"eRT.","Device.LogAgent.X_RDKCENTRAL-COM_LogLevel");
+    LEDMANAGER_RDKLogEnable = GetLogInfo(bus_handle,"eRT.","Device.LogAgent.X_RDKCENTRAL-COM_LoggerEnable");
+    LEDMANAGER_RDKLogLevel = (char)GetLogInfo(bus_handle,"eRT.","Device.LogAgent.X_RDKCENTRAL-COM_LogLevel");
+
+    while ( cmdChar != 'q' )
+    {
+        cmdChar = getchar();
+
+        cmd_dispatch(cmdChar);
+    }
+
+#elif defined(_ANSC_LINUX)
+    if ( bRunAsDaemon )
+        daemonize();
+
+    CcspTraceInfo(("\nAfter daemonize before signal\n"));
+
+#ifdef INCLUDE_BREAKPAD
+    breakpad_ExceptionHandler();
+#else
+    signal(SIGTERM, sig_handler);
+    signal(SIGINT, sig_handler);
+    /*signal(SIGCHLD, sig_handler);*/
+    signal(SIGUSR1, sig_handler);
+    signal(SIGUSR2, sig_handler);
+
+    signal(SIGSEGV, sig_handler);
+    signal(SIGBUS, sig_handler);
+    signal(SIGKILL, sig_handler);
+    signal(SIGFPE, sig_handler);
+    signal(SIGILL, sig_handler);
+    signal(SIGQUIT, sig_handler);
+    signal(SIGHUP, sig_handler);
+#endif
+
+    cmd_dispatch('e');
+
+    char *subSys = NULL;
+#ifdef _COSA_SIM_
+    subSys = "";        /* PC simu use empty string as subsystem */
+#else
+    subSys = NULL;      /* use default sub-system */
+#endif
+
+    int err;
+    err = Cdm_Init(bus_handle, subSys, NULL, NULL, pComponentName);
+    if (err != CCSP_SUCCESS)
+    {
+        fprintf(stderr, "Cdm_Init: %s\n", Cdm_StrError(err));
+        exit(1);
+    }
+
+    ledmgr_start();
+
+    if ( bRunAsDaemon )
+    {
+        while(1)
+        {
+            sleep(30);
+        }
+    }
+    else
+    {
+        while ( cmdChar != 'q' )
+        {
+            cmdChar = getchar();
+
+            cmd_dispatch(cmdChar);
+        }
+    }
+
+#endif
+
+    return SUCCESS;
+
+}
---
+++ source/led_manager.h
@@ -0,0 +1,103 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2020 Sky
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * Copyright [2014] [Cisco Systems, Inc.]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef _LEDMGR_H_
+#define _LEDMGR_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <json-c/json.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include "ccsp_trace.h"
+#include "platform_hal.h"
+
+#define COMPONENT_NAME_LEDMANAGER    "com.cisco.spvtg.ccsp.ledmanager"
+#define COMPONENT_PATH_LEDMANAGER    "/com/cisco/spvtg/ccsp/ledmanager"
+#define COMPONENT_VERSION_LEDMANAGER 1
+
+#define SUCCESS                      0
+#define FAILURE                      -1
+
+#define BUFLEN_4                     4           //!< buffer length 4
+#define BUFLEN_8                     8           //!< buffer length 8
+#define BUFLEN_16                    16          //!< buffer length 16
+#define BUFLEN_18                    18          //!< buffer length 18
+#define BUFLEN_24                    24          //!< buffer length 24
+#define BUFLEN_32                    32          //!< buffer length 32
+#define BUFLEN_40                    40          //!< buffer length 40
+#define BUFLEN_48                    48          //!< buffer length 48
+#define BUFLEN_64                    64          //!< buffer length 64
+#define BUFLEN_80                    80          //!< buffer length 80
+#define BUFLEN_128                   128         //!< buffer length 128
+#define BUFLEN_256                   256         //!< buffer length 256
+#define BUFLEN_264                   264         //!< buffer length 264
+#define BUFLEN_512                   512         //!< buffer length 512
+#define BUFLEN_1024                  1024        //!< buffer length 1024
+
+typedef enum {
+ePowerOn = 0,
+ePowerOff,
+eProximity,
+eDslTraining,
+eWanLinkUp,
+eWanLinkDown,
+eIPv4Up,
+eIPv4Down,
+eIPv6Up,
+eIPv6Down,
+eMaptUp,
+eMaptDown,
+eFwUpdateStart,
+eFwUpdateStop,
+eFwUpdateComplete,
+eVoiceProvisioned,
+eVoiceUp,
+eVoiceRinging,
+eVoiceError,
+eVoiceDown,
+eWiFiUp,
+eWiFiDown,
+eWpsStart,
+eWpsOverlap,
+eWpsTimeout,
+eWpsStop,
+eTimeOut,
+MAX_EVENTS
+}cpe_event_t;
+
+#endif /* _LEDMGR_H_ */ 
