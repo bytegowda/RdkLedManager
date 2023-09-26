f20719dd7f330c6e6f937de782a34b3f00a69a87
Annie Rashmitha <annie.rashmitha@sky.uk>
2023-09-26 11:16:08 +0000

    RDKB-51612: Opensourcing RdkLedManager
    Reason for change: Opensourcing RdkLedManager
    Test Procedure: Opensourcing RdkLedManager
    Risks: Low
    Signed-off-by: Annie Rashmitha <annie.rashmitha@sky.uk>
    
    Change-Id: I9a973db5c4f1c8b1a771d4c52b69b887efe5e3c1
    (cherry picked from commit d5b4e02b2b8164d7d420e68b5ccc5898922a0219)

--- source/led_manager.h
+++ source/led_manager.h
@@ -53,21 +53,21 @@
 #define SUCCESS                      0
 #define FAILURE                      -1
 
-#define BUFLEN_4                     4           //!< buffer length 4
-#define BUFLEN_8                     8           //!< buffer length 8
-#define BUFLEN_16                    16          //!< buffer length 16
-#define BUFLEN_18                    18          //!< buffer length 18
-#define BUFLEN_24                    24          //!< buffer length 24
-#define BUFLEN_32                    32          //!< buffer length 32
-#define BUFLEN_40                    40          //!< buffer length 40
-#define BUFLEN_48                    48          //!< buffer length 48
-#define BUFLEN_64                    64          //!< buffer length 64
-#define BUFLEN_80                    80          //!< buffer length 80
-#define BUFLEN_128                   128         //!< buffer length 128
-#define BUFLEN_256                   256         //!< buffer length 256
-#define BUFLEN_264                   264         //!< buffer length 264
-#define BUFLEN_512                   512         //!< buffer length 512
-#define BUFLEN_1024                  1024        //!< buffer length 1024
+#define BUFLEN_4                     4
+#define BUFLEN_8                     8
+#define BUFLEN_16                    16
+#define BUFLEN_18                    18
+#define BUFLEN_24                    24
+#define BUFLEN_32                    32
+#define BUFLEN_40                    40
+#define BUFLEN_48                    48
+#define BUFLEN_64                    64
+#define BUFLEN_80                    80
+#define BUFLEN_128                   128
+#define BUFLEN_256                   256
+#define BUFLEN_264                   264
+#define BUFLEN_512                   512
+#define BUFLEN_1024                  1024
 
 typedef enum {
 ePowerOn = 0,
